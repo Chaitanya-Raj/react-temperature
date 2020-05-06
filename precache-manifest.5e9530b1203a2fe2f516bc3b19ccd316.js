af606b40aa46a3d94037a57eb4151b9a99798757
Chaitanya Raj <chaitanyarajdixit@gmail.com>
2020-05-06 14:21:58 +0530

    Updates

---
+++ precache-manifest.5e9530b1203a2fe2f516bc3b19ccd316.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "671510a07cfc5f278b8b8dee57129b90",
+    "url": "/react-temperature/index.html"
+  },
+  {
+    "revision": "b8d0895f2c6fb9433e4d",
+    "url": "/react-temperature/static/css/main.381358d2.chunk.css"
+  },
+  {
+    "revision": "b81cad0ef280014cc560",
+    "url": "/react-temperature/static/js/2.12065831.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-temperature/static/js/2.12065831.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8d0895f2c6fb9433e4d",
+    "url": "/react-temperature/static/js/main.efdba99a.chunk.js"
+  },
+  {
+    "revision": "64d91d6a36e5d1c7d659",
+    "url": "/react-temperature/static/js/runtime-main.8f7a256f.js"
+  }
+]);
